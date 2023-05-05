939ee46231d7fb3286ad3788fc69d2e7109d0135
lexan4ik <48456739+lexan4ik@users.noreply.github.com>
2023-05-06 01:01:09 +0300

    Update functions_admin_chat.py

--- functions_admin_chat.py
+++ functions_admin_chat.py
@@ -24,8 +24,10 @@ def order_admin_list(id_order,num):
         num = 0
     try:
         for i in result:
+            user_info = BotDB.info_user(result[num][9])
             cart_add = InlineKeyboardMarkup(row_width=4)
-            text = f"Заказ N{result[num][8]}\n\n{result[num][2]} ({result[num][1]})\n\nКоличество: {result[num][5]}\n\n{result[num][3]} byn\n\nСтатус: {result[num][6]}\n\n{result[num][7]}"
+            text_info = f'Имя: {user_info[0]}\n\nФамилия: {user_info[1]}\n\nАдрес доставки: {user_info[2]}\n\nНомер телефона: {user_info[3]}\n\n'
+            text = f"Заказ N{result[num][8]}  от {result[num][9]}\n\n{result[num][2]} ({result[num][1]})\n\nКоличество: {result[num][5]}\n\n{result[num][3]} byn\n\nСтатус: {result[num][6]}\n\n{result[num][7]}"
             photo = result[num][4]
 
             if len(result) - 1 == 0 and num == 0:
